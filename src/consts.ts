9d9a2353f9361a0683021f413a3aabedec29751a
mahesh-143 <maheshodedara13@yahoo.com>
2025-02-21 17:55:52 +0530

    changed colorscheme

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 import type { Metadata, Site, Socials } from "@types";
 
 export const SITE: Site = {
-  TITLE: "Mahesh Odedara",
+  TITLE: "Mahesh's blog",
   DESCRIPTION: "Software Developer for fun",
   EMAIL: "maheshodedara13@yahoo.com",
   NUM_POSTS_ON_HOMEPAGE: 5,
